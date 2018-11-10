7648b67d0a84f203354b0274cd9e45ce5dfcff36
Ross Andrews <ross.andrews@gmail.com>
2018-11-10 01:53:44 -0600

    lcd_clear function

--- lcd.asm
+++ lcd.asm
@@ -1,19 +1,74 @@
-.ctrl_reset: equ 10000000b ; clear for reset
-.ctrl_e:     equ 01000000b ; set, then falling edge, to latch
-.ctrl_rw:    equ 00100000b ; set for input
-.ctrl_rs:    equ 00010000b ; set for data
-.led2:       equ 00001000b
-.led1:       equ 00000100b
-.io_write:   equ 10000010b
-.io_read:    equ 10010010b
-.addr_data:  equ 0
-.addr_btns:  equ 1
-.addr_ctrl:  equ 2
-.addr_dir:   equ 3
+.ctrl_reset:  equ 10000000b ; clear for reset
+.ctrl_e:      equ 01000000b ; set, then falling edge, to latch
+.ctrl_rw:     equ 00100000b ; set for input
+.ctrl_rs:     equ 00010000b ; set for data
+.led2:        equ 00001000b
+.led1:        equ 00000100b
+.io_write:    equ 10000010b
+.io_read:     equ 10010010b
+.addr_data:   equ 0
+.addr_btns:   equ 1
+.addr_ctrl:   equ 2
+.addr_dir:    equ 3
+
+.ram_base:     equ 0x8000
+.pixel_buffer: equ .ram_base ; Enough bits for 1 bpp for the whole screen
+.scratch:      equ .pixel_buffer+(128*8) ; 32 bytes of scratch stuff
 
 	org 0000
+
 	call lcd_reset
+	call lcd_clear
+
 	halt
+
+include "tiles.asm"
+
+; x in b, y in c
+.setxy:
+	push af
+	ld a,c
+	cp 32
+	jr c,.tophalf
+	sub 32
+	or 0x80
+	call .writecmd
+	ld a,b
+	or 0x88
+	call .writecmd
+	jr .setxy_done
+.tophalf:
+	or 0x80
+	call .writecmd
+	ld a,b
+	or 0x80
+	call .writecmd
+.setxy_done:
+	pop af
+	ret
+
+; Sets a tile on the grid: tile addr in hl, x in b, y in c
+set_tile:
+	ret
+
+lcd_clear:
+	push af
+	push bc
+	ld c,64 ; Row we're on
+.clearrow:
+	ld b,0
+	call .setxy
+	ld b,16
+.send_empty:
+	ld a,0x55
+	call .writedata
+	djnz .send_empty
+	dec c
+	bit 7,c
+	jr z,.clearrow
+	pop bc
+	pop af
+	ret
 
 lcd_reset:
 	push af
